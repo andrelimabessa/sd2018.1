09c05f3432730d34e6da3e710371f42e959e3b90
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-06-23 20:23:37 -0300

    Adicionando o atributo vizinhos

--- django/projeto/app/models.py
+++ django/projeto/app/models.py
@@ -4,4 +4,5 @@ from django.utils import timezone
 class Jogada(models.Model):
     linha = models.CharField(max_length=2)
     coluna = models.CharField(max_length=2)
+    vizinhos = 0
     created_date = models.DateTimeField(default=timezone.now)
