413a93d5f296c8db7e9294896493c7757b24ac14
Marcio Magalhaes <marcio.mag@outlook.com>
2018-06-23 14:17:18 -0300

    VF Django - Funcionando basico: Entrando com valores

--- VF/app/views.py
+++ VF/app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Jogada
+from .forms import JogadaForm
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'app/post_list.html', {})
+    if request.method == "POST":
+        form = JogadaForm(request.POST)
+        if form.is_valid():
+            jogada = form.save(commit=False)
+            jogada.created_date = timezone.now()
+            jogada.save()
+    else:
+        form = JogadaForm()
+
+    jogadas = Jogada.objects.all()
+
+    return render(request, 'app/post_list.html', {'form': form, 'jogadas':jogadas})
