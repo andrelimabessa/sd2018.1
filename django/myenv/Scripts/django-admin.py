f38f91f9cc8e5e7eb14f07c82e1622509d983598
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-06-23 12:00:35 -0300

    versao inicial

---
+++ django/myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fabian~1\docume~1\reposi~1\sd2018.1\django\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
