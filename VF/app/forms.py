58669f7fe96c14a4e6732bfbc95d01e669a661cb
Marcio Magalhaes <marcio.mag@outlook.com>
2018-06-24 22:33:43 -0300

    VF Django mudancas de Layout

--- VF/app/forms.py
+++ VF/app/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from .models import Jogada
 
 class JogadaForm(forms.Form):
-    linha = forms.IntegerField(label='Linha')
-    coluna = forms.IntegerField(label='Coluna')
+    linha = forms.IntegerField(label = 'Linha')
+    coluna = forms.IntegerField(label = 'Coluna')
